28870a1468fb0daafed6df064ad8edfea8eee3b0
MEKKTSX <kittisakworkshop@gmail.com>
2026-02-13 19:03:11 +0700

    Update CACHE_NAME to esb-thai-v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'esb-thai-v3';
+const CACHE_NAME = 'esb-thai-v4';
 const urlsToCache = ['./', './index.html', './manifest.json', './icon.png'];
 
 self.addEventListener('install', (e) => {
